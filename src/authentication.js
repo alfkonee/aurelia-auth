c6c091210988a081ab63f8b4ffd3b829746bde0f
Alfred Neequaye <alfkonee@hotmail.com>
2016-01-23 20:41:21 +0000

    Tab Size VSCODE

--- src/authentication.js
+++ src/authentication.js
@@ -3,41 +3,41 @@ import {BaseConfig}  from './baseConfig';
 import {Storage} from './storage';
 import authUtils from './authUtils';
 @inject(Storage, BaseConfig)
-export class Authentication{
-  constructor( storage, config){
+export class Authentication {
+  constructor(storage, config) {
     this.storage = storage;
     this.config = config.current;
-        this.tokenName = this.config.tokenPrefix ? this.config.tokenPrefix + '_'
-            + this.config.tokenName : this.config.tokenName;
-        this.refreshTokenName = this.config.refreshTokenPrefix ? this.config.refreshTokenPrefix + '_'
-            + this.config.refreshTokenName : this.config.refreshTokenName;
+    this.tokenName = this.config.tokenPrefix ? this.config.tokenPrefix + '_'
+      + this.config.tokenName : this.config.tokenName;
+    this.refreshTokenName = this.config.refreshTokenPrefix ? this.config.refreshTokenPrefix + '_'
+      + this.config.refreshTokenName : this.config.refreshTokenName;
   }
-    getLoginRoute() {
+  getLoginRoute() {
     return this.config.loginRoute;
   }
 
-    getLoginRedirect() {
+  getLoginRedirect() {
     return this.config.loginRedirect;
   }
-  
-  getLoginUrl() {
-        return this.config.baseUrl ? authUtils.joinUrl(this.config.baseUrl, this.config.loginUrl) : this.config.loginUrl;
-    };
 
-    getSignupUrl() {
-        return this.config.baseUrl ? authUtils.joinUrl(this.config.baseUrl, this.config.signupUrl) : this.config.signupUrl;
-    };
+  getLoginUrl() {
+    return this.config.baseUrl ? authUtils.joinUrl(this.config.baseUrl, this.config.loginUrl) : this.config.loginUrl;
+  };
+
+  getSignupUrl() {
+    return this.config.baseUrl ? authUtils.joinUrl(this.config.baseUrl, this.config.signupUrl) : this.config.signupUrl;
+  };
 
   getProfileUrl() {
-        return this.config.baseUrl ? authUtils.joinUrl(this.config.baseUrl, this.config.profileUrl) : this.config.profileUrl;
-    };
+    return this.config.baseUrl ? authUtils.joinUrl(this.config.baseUrl, this.config.profileUrl) : this.config.profileUrl;
+  };
 
   getToken() {
     return this.storage.get(this.tokenName);
-    };
-    getRefreshToken(){
-        return this.storage.get(this.refreshTokenName);
-    }
+  };
+  getRefreshToken() {
+    return this.storage.get(this.refreshTokenName);
+  }
   getPayload() {
     var token = this.storage.get(this.tokenName);
 
@@ -64,14 +64,14 @@ export class Authentication{
 
     if (!token && response) {
       token = this.config.tokenRoot && response.content[this.config.tokenRoot]
-      ? response.content[this.config.tokenRoot][this.config.tokenName]
-      : response.content[this.config.tokenName];
+        ? response.content[this.config.tokenRoot][this.config.tokenName]
+        : response.content[this.config.tokenName];
     }
 
     if (!token) {
       var tokenPath = this.config.tokenRoot
-      ? this.config.tokenRoot + '.' + this.config.tokenName
-      : this.config.tokenName;
+        ? this.config.tokenRoot + '.' + this.config.tokenName
+        : this.config.tokenName;
 
       throw new Error('Expecting a token named "' + tokenPath + '" but instead got: ' + JSON.stringify(response.content));
     }
@@ -80,43 +80,43 @@ export class Authentication{
     this.storage.set(tokenName, token);
 
     if (this.config.loginRedirect && !redirect) {
-      window.location.href =this.config.loginRedirect;
+      window.location.href = this.config.loginRedirect;
     } else if (redirect && authUtils.isString(redirect)) {
-            window.location.href = window.encodeURI(redirect);
-        }
+      window.location.href = window.encodeURI(redirect);
+    }
+  }
+
+  setRefreshToken(response) {
+    var refreshTokenName = this.refreshTokenName;
+    var refreshToken = response && response.refresh_token;
+    var token;
+
+    if (refreshToken) {
+      if (authUtils.isObject(refreshToken) && authUtils.isObject(refreshToken.data)) {
+        response = refreshToken;
+      } else if (authUtils.isString(refreshToken)) {
+        token = refreshToken;
+      }
     }
 
-    setRefreshToken(response) {
-        var refreshTokenName = this.refreshTokenName;
-        var refreshToken = response && response.refresh_token;
-        var token;
-
-        if (refreshToken) {
-            if (authUtils.isObject(refreshToken) && authUtils.isObject(refreshToken.data)) {
-                response = refreshToken;
-            } else if (authUtils.isString(refreshToken)) {
-                token = refreshToken;
-            }
-        }
-
-        if (!token && response) {
-            token = this.config.refreshTokenRoot && response.content[this.config.refreshTokenRoot]
-                ? response.content[this.config.refreshTokenRoot][this.config.refreshTokenName]
-                : response.content[this.config.refreshTokenName];
-        }
-
-        if (!token) {
-            var refreshTokenPath = this.config.refreshTokenRoot
-                ? this.config.refreshTokenRoot + '.' + this.config.refreshTokenName
-                : this.config.refreshTokenName;
-
-            throw new Error('Expecting a refresh token named "' + refreshTokenPath + '" but instead got: ' + JSON.stringify(response.content));
-        }
-        
-        this.storage.set(refreshTokenName, token);       
+    if (!token && response) {
+      token = this.config.refreshTokenRoot && response.content[this.config.refreshTokenRoot]
+        ? response.content[this.config.refreshTokenRoot][this.config.refreshTokenName]
+        : response.content[this.config.refreshTokenName];
     }
 
-    removeToken() {
+    if (!token) {
+      var refreshTokenPath = this.config.refreshTokenRoot
+        ? this.config.refreshTokenRoot + '.' + this.config.refreshTokenName
+        : this.config.refreshTokenName;
+
+      throw new Error('Expecting a refresh token named "' + refreshTokenPath + '" but instead got: ' + JSON.stringify(response.content));
+    }
+
+    this.storage.set(refreshTokenName, token);
+  }
+
+  removeToken() {
     this.storage.remove(this.tokenName);
   }
 
@@ -133,28 +133,28 @@ export class Authentication{
       return true;
     }
 
-        var base64Url = token.split('.')[1];
-        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-        var exp = JSON.parse(window.atob(base64)).exp;
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var exp = JSON.parse(window.atob(base64)).exp;
 
-        if (exp) {
-          return Math.round(new Date().getTime() / 1000) <= exp;
-        }
-
-      return true;
+    if (exp) {
+      return Math.round(new Date().getTime() / 1000) <= exp;
     }
+
+    return true;
+  }
 
   logout(redirect) {
     return new Promise(resolve => {
       this.storage.remove(this.tokenName);
 
-if (this.config.logoutRedirect && !redirect) {
-  window.location.href = this.config.logoutRedirect;
+      if (this.config.logoutRedirect && !redirect) {
+        window.location.href = this.config.logoutRedirect;
       } else if (authUtils.isString(redirect)) {
         window.location.href = redirect;
-}
+      }
 
-resolve();
-});
+      resolve();
+    });
   }
 }
